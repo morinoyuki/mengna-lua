ef8bc9b76ead8f4fdd734d257092152c9f84f1b0
morinoyuki <shinshimumei@gmail.com>
2020-03-25 10:51:45 +0800

    舔狗日志

--- events/GroupAddInvite.lua
+++ events/GroupAddInvite.lua
@@ -1,6 +1,9 @@
+import("Native.Csharp.Sdk","Native.Csharp.Sdk.Cqp.Enum")
+
+ 
 return function (data)
     if data.qq == Utils.setting.AdminQQ then--开发者邀请
-        CQApi:SetGroupAddRequest(data.tag,2,1,"同意邀请操作")--同意邀请
+        CQApi:SetGroupAddRequest(data.tag,CQGroupAddRequestType.RobotBeInviteAddGroup,CQResponseType.PASS,"同意邀请操作")--同意邀请
     end
-    CQApi:SetGroupAddRequest(data.tag,2,2,"不加新群"..tostring(data.qq))
+    -- CQApi:SetGroupAddRequest(data.tag,2,2,"不加新群"..tostring(data.qq))
 end
